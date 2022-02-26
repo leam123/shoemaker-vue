a22630cef133441c10c1221434f3d3d92fab39a0
leam123 <garcia.leamor1@gmail.com>
2022-02-26 15:15:27 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://shoemakerapi.herokuapp.com/'
+axios.defaults.baseURL = 'https://shoemakerapi.herokuapp.com'
 
 createApp(App).use(store).use(router, axios).mount('#app')
